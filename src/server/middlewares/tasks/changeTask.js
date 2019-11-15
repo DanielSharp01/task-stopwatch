afc12eb90fb96ece5e0fc8ec3cd25b01895b5f91
DanielSharp01 <DanielSharp01@gmail.com>
2019-11-15 15:09:38 +0100

    Fixed change task bug

--- src/server/middlewares/tasks/changeTask.js
+++ src/server/middlewares/tasks/changeTask.js
@@ -8,9 +8,11 @@ export default objectRepository => async (req, res, next) => {
   if (req.body.start) res.locals.task.start = new Date(req.body.start);
   if (typeof req.body.stop !== "undefined") res.locals.task.stop = req.body.stop && new Date(req.body.stop);
   
-  if (req.body.start > req.body.stop) return next({ status: 400, message: "Task start must be before stop" });
-  const overlaps = await Task.find({ userId: req.userId, $and: [ { stop: { $gt: req.body.start } }, { start: { $lt: req.body.stop } } ] });
-  if (overlaps.length > 0) return next({ status: 400, message: "Tasks cannot overlap" });
+  if (req.body.start) {
+    if (req.body.start > req.body.stop) return next({ status: 400, message: "Task start must be before stop" });
+    const overlaps = await Task.find({ userId: req.userId, $and: [ { stop: { $gt: req.body.start } }, { start: { $lt: req.body.stop } } ] });
+    if (overlaps.length > 0) return next({ status: 400, message: "Tasks cannot overlap" });
+  }
 
   if (typeof req.body.tags !== "undefined") {
     let resolvedTags = [];
