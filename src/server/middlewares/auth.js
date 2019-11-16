861761c760d4f84be7629e8ccbc12c61953a0996
DanielSharp01 <DanielSharp01@gmail.com>
2019-11-16 14:55:01 +0100

    Added API authentication

--- src/server/middlewares/auth.js
+++ src/server/middlewares/auth.js
@@ -1,13 +1,20 @@
-const url = require("url");
 const jwt = require("jsonwebtoken");
-const mongoose = require("mongoose");
 
 function checkToken(req) {
   if (req.cookies.token) {
     try {
       let decoded = jwt.verify(req.cookies.token, process.env.SECRET);
-      if (decoded) return { authed: true, userId: decoded.id };
+      if (decoded) return { authed: true, withCookie: true, userId: decoded.id };
     } catch (err) {}
+  }
+  else if (req.headers.authorization) {
+    const parts = req.headers.authorization.split(' ');
+    if (parts.length == 2 && /^Bearer$/i.test(parts[0])) {
+      try {
+        let decoded = jwt.verify(parts[1], process.env.SECRET);
+        if (decoded) return { authed: true, withCookie: false, userId: decoded.id };
+      } catch (err) {}
+    } 
   }
   return { authed: false };
 }
@@ -17,11 +24,11 @@ function refreshCookie(req, res) {
 }
 
 module.exports = (setRedirect = true, redirect = true) => (req, res, next) => {
-  const { authed, userId } = checkToken(req);
+  const { authed, userId, withCookie } = checkToken(req);
   req.authed = authed;
   if (req.authed) {
-    req.userId = new mongoose.Types.ObjectId(userId);
-    refreshCookie(req, res);
+    req.userId = userId;
+    if (withCookie) refreshCookie(req, res);
     return next();
   } else {
     if (setRedirect && req.session) req.session.redirectUrl = req.originalUrl;
