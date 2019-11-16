257da555408cee6a140693d752c529954ec7e135
DanielSharp01 <DanielSharp01@gmail.com>
2019-11-16 18:21:29 +0100

    Auth middleware bugfix

--- src/server/middlewares/auth.js
+++ src/server/middlewares/auth.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
 function checkToken(req) {
   if (req.cookies.token) {
@@ -27,7 +28,7 @@ module.exports = (setRedirect = true, redirect = true) => (req, res, next) => {
   const { authed, userId, withCookie } = checkToken(req);
   req.authed = authed;
   if (req.authed) {
-    req.userId = userId;
+    req.userId = new mongoose.Types.ObjectId(userId);;
     if (withCookie) refreshCookie(req, res);
     return next();
   } else {
